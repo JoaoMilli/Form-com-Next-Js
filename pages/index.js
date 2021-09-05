1486710c957edad8c9d4fa4a42006db9c1cdb82f
MillJoao <pedromilli123@gmail.com>
2021-09-05 16:58:28 -0300

    Limpeza de arquivos

--- pages/index.js
+++ pages/index.js
@@ -5,40 +5,6 @@ import UserForm from './components/UserForm'
 
 export default function Home() {
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
-  const [submitted, setSubmitted] = useState(false)
-
-  const handleSubmit = (e) => { 
-    e.preventDefault()
-    console.log('Sending')
-
-    let data = {
-        name,
-        email,
-        message
-    }
-
-    fetch('/api/contact', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then((res) => {
-      console.log('Response received')
-      if (res.status === 200) {
-        console.log('Response succeeded!')
-        setSubmitted(true)
-        setName('')
-        setEmail('')
-        setMessage('')
-      }
-    })
-  }
-
   return (
     <UserForm/>
   )
